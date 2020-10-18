3d44206d6883cde8285d424c9ba1143dc1e5519b
Suraj1199 <https://github.com/Suraj1199>
2020-10-18 17:51:19 +0530

    Add Hello World Program

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+class HelloWorld {
+
+	public void sayHello() {
+		System.out.println("Hello World\n");
+	}
+}
+
+class Main {
+
+	public static void main(String args[]) {
+		HelloWorld ob = new HelloWorld();
+		ob.sayHello();
+	}
+}
